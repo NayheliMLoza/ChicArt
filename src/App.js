1b73e76a3bf608d0bec31825d7caf23ec522980b
nay <>
2023-10-18 19:14:45 -0300

    PreEntrega1-Loza

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import './App.css';
-
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/navbar/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-      
       <NavBar/>
-      
+      <ItemListContainer greeting="articulos disponibles"/>
     </div>
   );
 }
